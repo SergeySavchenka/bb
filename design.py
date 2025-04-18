09f2ac44ccfb13de53f677e58b837673763c160e
SergeySavchenka <118177405+SergeySavchenka@users.noreply.github.com>
2025-04-18 09:54:52 +0300

    Create design.py

---
+++ design.py
@@ -0,0 +1,31 @@
+Form.setStyleSheet("""
+                    #Form {
+                        background-color: rgb(181, 213, 202);
+                    }
+                    QPushButton {
+                        background-color: rgb(224, 169, 175);
+                        border: none;
+                        border-radius: 6px;
+                        font-size: 12pt;
+                        color: rgb(0, 0, 0);
+                        font-family: "MS Shell Dlg 2";
+                    }
+                    QPushButton:hover {
+                        background-color: rgb(200, 150, 155);
+                    }
+                    QLabel {
+                        font-size: 14pt;
+                        color: rgb(0, 0, 0);
+                        font-family: "MS Shell Dlg 2";
+                    }
+                    QLineEdit {
+                        background-color: rgb(255, 255, 255);
+                        border-radius: 6px;
+                        font-size: 12pt;
+                        color: rgb(0, 0, 0);
+                        font-family: "MS Shell Dlg 2";
+                        padding: 5px;
+
+                    QMessage
+                    }
+                """)
